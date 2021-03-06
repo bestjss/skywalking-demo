9b57ec2e15a8e49535b4682d29249b421e93e870
jiasi.duan <jiasi.duan@oceanotech.com>
2021-03-06 17:32:38 +0800

    update

--- server-c/index.js
+++ server-c/index.js
@@ -1,3 +1,13 @@
+require('skyapm-nodejs').start({
+    // Service name is showed in sky-walking-ui. Suggestion: set an unique name for each service, one
+    // service's nodes share the same code.
+    // this value cannot be empty.
+    serviceName: 'server-c',
+    // The identifier of the instance
+    // Collector agent_gRPC/grpc service addresses.
+    // default value: localhost:11800
+    directServers: '127.0.0.1:11800'
+});
 const koa = require('koa');
 const router = require('koa-router')();
 const bodyParser = require('koa-bodyparser');
@@ -14,4 +24,5 @@ app.use(bodyParser());
 app.use(route.routes());
 
 app.listen(8083);
+
 console.log('Server C started at port 8083...');
