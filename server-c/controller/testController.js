49d41e79de08f569dc45b9c259ec319966621cc2
jiasi.duan <jiasi.duan@oceanotech.com>
2021-03-06 21:56:10 +0800

    update

--- server-c/controller/testController.js
+++ server-c/controller/testController.js
@@ -1,17 +1,45 @@
+const request = require('request')
 
 class TestController {
-    async getMethodA(ctx, next){
+    async getMethodA(ctx, next) {
         ctx.body = 'this is Server C Method A'
     }
 
-    async getMethodB(ctx, next){
+    async getMethodB(ctx, next) {
         let sleep = ctx.params.sleep;
         ctx.body = 'this is Server C Method B' + sleep
     }
 
-    async getMethodC(ctx, next){
+    async getMethodC(ctx, next) {
         ctx.body = 'this is Server C Method C'
     }
+
+    async getMethodAD(ctx, next) {
+        ctx.body =await get('http://localhost:8084/test/method-a')
+    }
+
+    async getMethodBD(ctx, next) {
+        let sleep = ctx.params.sleep;
+        ctx.body =await get('http://localhost:8084/test/method-b/' + sleep)
+    }
+
+    async getMethodCD(ctx, next) {
+        ctx.body =await get('http://localhost:8084/test/method-c')
+    }
+
+
 }
 
-module.exports= new TestController();
+const get = url =>
+    new Promise((resolve, reject) =>
+        request.get(url, (err, response, body) => {
+            if (!err && response.statusCode == 200) {
+                console.log(body);
+                resolve(body);
+            } else {
+                reject(err);
+            }
+        })
+    )
+
+module.exports = new TestController();
--- server-c/router/index.js
+++ server-c/router/index.js
@@ -5,6 +5,9 @@ router.prefix('/test');
 router.get('/method-a',testCtl.getMethodA);
 router.get('/method-b/:sleep',testCtl.getMethodB);
 router.get('/method-c',testCtl.getMethodC);
+router.get('/d/method-a',testCtl.getMethodAD);
+router.get('/d/method-b/:sleep',testCtl.getMethodBD);
+router.get('/d/method-c',testCtl.getMethodCD);
 
 module.exports= router;
 
